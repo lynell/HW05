c0834ca13fa6f62a9ad8cc362d052ab99358ee41
Lynell <lynellamanna@live.com>
2015-08-11 01:51:45 +0530

    Completed 1st and 2nd of day 5

--- HW05_ex00_TextAdventure.py
+++ HW05_ex00_TextAdventure.py
@@ -2,79 +2,81 @@
 # HW05_ex00_TextAdventure.py
 ##############################################################################
 # Imports
-from sys import exit
+import sys
 
 # Body
 
 
-def infinite_stairway_room(count=0):
-    print "You walk through the door to see a dimly lit hallway."
+def infinite_stairway_room(user,count=0):
+    print "%s walk through the door to see a dimly lit hallway." %user
     print "At the end of the hallway is a", count * 'long ', 'staircase going towards some light'
     next = raw_input("> ")
     
     # infinite stairs option
     if next == "take stairs":
-        print 'You take the stairs'
+        print '%s take the stairs'%user
         if (count > 0):
             print "but you're not happy about it"
-        infinite_stairway_room(count + 1)
+        infinite_stairway_room(user,count + 1)
     # option 2 == ?????
-    if next == option_2:
-        pass
+    if next == "run":
+        dead("Try again")
+        
 
 
-def gold_room():
+def gold_room(user):
     print "This room is full of gold.  How much do you take?"
 
     next = raw_input("> ")
     if "0" in next or "1" in next:
-        how_much = int(next)
-    else:
-        dead("Man, learn to type a number.")
-
-    if how_much < 50:
-        print "Nice, you're not greedy, you win!"
-        exit(0)
+        try:
+            how_much = int(next)
+        except:
+            dead("Man, learn to type a number.")
+        else:
+            if how_much < 50:
+                print "Nice, you're not greedy, you win!"
+                exit(0)
     else:
         dead("You greedy bastard!")
 
 
-def bear_room():
+def bear_room(user):
     print "There is a bear here."
     print "The bear has a bunch of honey."
     print "The fat bear is in front of another door."
-    print "How are you going to move the bear?"
+    print "How is %s going to move the bear?" %user
     bear_moved = False
 
     while True:
-        next = raw_input("> ")
+        next = raw_input(">")
 
-        if next == "take honey":
+        if ("take" in next) or ("honey" in next) :
             dead("The bear looks at you then slaps your face off.")
-        elif next == "taunt bear" and not bear_moved:
+        elif ("taunt" in next) and not bear_moved:
             print "The bear has moved from the door. You can go through it now."
             bear_moved = True
-        elif next == "taunt bear" and bear_moved:
+        elif "taunt" in next and bear_moved:
             dead("The bear gets pissed off and chews your leg off.")
-        elif next == "open door" and bear_moved:
-            gold_room()
+        elif (("open" in next) or("door" in next)) and bear_moved:
+            gold_room(user)
         else:
             print "I got no idea what that means."
 
 
-def cthulhu_room():
-    print "Here you see the great evil Cthulhu."
-    print "He, it, whatever stares at you and you go insane."
-    print "Do you flee for your life or eat your head?"
+def cthulhu_room(user):
+    print "Here ,%s you see the great evil Cthulhu." %user
+    print "He, it, whatever stares at %s and you go insane." %user
+    print "Do you, %s flee for your life or eat your head?" %user
 
-    next = raw_input("> ")
+    next = raw_input(">")
 
     if "flee" in next:
-        start()
+        main()
     elif "head" in next:
         dead("Well that was tasty!")
     else:
-        cthulhu_room()
+        cthulhu_room(user)
 
 
 def dead(why):
@@ -83,20 +85,24 @@ def dead(why):
 
 
 ##############################################################################
-def start():
+def main():
     # START the TextAdventure game
-    print "You are in a dark room."
+    #print "What is your name?.."
+    user = sys.argv[1]
+    print user  + ",you are in a dark room."
     print "There is a door to your right and left."
     print "Which one do you take?"
 
-    next = raw_input("> ")
+    next = raw_input("Enter left or right ")
 
     if next == "left":
-        bear_room()
+        bear_room(user)
     elif next == "right":
-        cthulhu_room()
+        cthulhu_room(user)
+    elif next=="up":
+        infinite_stairway_room(user)
     else:
         dead("You stumble around the room until you starve.")
 
 if __name__ == '__main__':
-    start()
+    main()
--- HW05_ex00_logics.py
+++ HW05_ex00_logics.py
@@ -11,18 +11,48 @@ def even_odd():
         Prints determination
         returns None
     """
-    pass
+    i=0
+    while i != 1:
+        try:
+            a = raw_input("Enter an integer: ")
+            b = int(a)
+        except:
+            print "Enter a number"
+        else:
+            i = 1
+            if b % 2 ==0:
+                print "The number is an even number"
+            else:
+                print "he number is odd"
+    
 
 
 def ten_by_ten():
-    """ Prints integers 1 through 100 sequentially in a ten by ten grid."""
-    pass
+    for i in range(101):
+        if i>0:
+            if i<10:
+                print "",str(i),
+                continue
+            if(i%10)==0:
+                print str(i)+ "\n"
+            else:
+                print i,
+    
 
 
 def find_average():
     """ Takes numeric input (non-word numerals) from the user, one number
     at a time. Once the user types 'done', returns the average.
     """
+    count = 0
+    sum = 0
+    a = raw_input("Enter a value: ")
+    while(a!= "done"):
+        sum+= int(a)
+        count+= 1
+        a = raw_input("enter another value")
+    avg = sum/float(count)    
+    print avg        
     pass
 
 ##############################################################################
@@ -33,6 +63,9 @@ def main():
     Prints the following function:
         - find_average()
     """
+    find_average()
+    ten_by_ten()
+    even_odd()
     pass
 
 if __name__ == '__main__':
